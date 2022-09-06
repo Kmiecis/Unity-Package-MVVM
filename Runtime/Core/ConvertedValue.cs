7c8a342c609cf6e633eaf3044d2be6e0659a8274
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-09-06 20:52:39 +0200

    ConvertedValue

---
+++ Runtime/Core/ConvertedValue.cs
@@ -0,0 +1,110 @@
+using System;
+
+namespace Common.MVB
+{
+    public static class ConvertedValue
+    {
+        public static ConvertedValue<TIn, TOut> Create<TIn, TOut>(Func<TIn, TOut> converter, IDynamicValue<TIn> value)
+        {
+            return new ConvertedValue<TIn, TOut>(converter, value);
+        }
+    }
+
+    public class ConvertedValue<TIn, TOut> : IDynamicValue<TOut>
+    {
+        protected readonly Func<TIn, TOut> _converter;
+        protected readonly IDynamicValue<TIn> _value;
+
+        protected event Action<TOut> _callback;
+
+        public ConvertedValue(Func<TIn, TOut> converter, IDynamicValue<TIn> value)
+        {
+            _converter = converter;
+            _value = value;
+        }
+
+        public TOut Value
+        {
+            get => GetValue();
+        }
+
+        public void AddListener(Action<TOut> callback)
+        {
+            if (_callback == null)
+            {
+                _value.AddListener(OnValueChanged);
+            }
+
+            _callback += callback;
+        }
+
+        public void RemoveListener(Action<TOut> callback)
+        {
+            _callback -= callback;
+
+            if (_callback == null)
+            {
+                _value.RemoveListener(OnValueChanged);
+            }
+        }
+
+        public void RemoveAllListeners()
+        {
+            _callback = null;
+            _value.RemoveListener(OnValueChanged);
+        }
+
+        public virtual TOut GetValue()
+        {
+            return _converter(_value.Value);
+        }
+
+        protected void Invoke(TOut value)
+        {
+            _callback?.Invoke(value);
+        }
+
+        protected void Invoke(TIn value)
+        {
+            Invoke(_converter(value));
+        }
+
+        public void Invoke()
+        {
+            Invoke(_value.Value);
+        }
+
+        protected void OnValueChanged(TIn value)
+        {
+            Invoke(value);
+        }
+
+        public static implicit operator TOut(ConvertedValue<TIn, TOut> value)
+        {
+            return value.Value;
+        }
+
+        private bool Equals(ConvertedValue<TIn, TOut> other)
+        {
+            return (
+                other != null &&
+                (ReferenceEquals(this, other) || Equals(this.Value, other.Value))
+            );
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ConvertedValue<TIn, TOut>);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+}
--- Runtime/Core/DynamicValue.cs
+++ Runtime/Core/DynamicValue.cs
@@ -14,11 +14,6 @@ namespace Common.MVB
         {
             return new DynamicValue<T>(value);
         }
-
-        public static DynamicValue<TIn, TOut> Create<TIn, TOut>(Func<TIn, TOut> converter, DynamicValue<TIn> value)
-        {
-            return new DynamicValue<TIn, TOut>(converter, value);
-        }
     }
 
     [Serializable]
@@ -96,104 +91,6 @@ namespace Common.MVB
         public override bool Equals(object obj)
         {
             return Equals(obj as DynamicValue<T>);
-        }
-
-        public override int GetHashCode()
-        {
-            return _value.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return _value.ToString();
-        }
-    }
-
-    public class DynamicValue<TIn, TOut> : IDynamicValue<TOut>
-    {
-        protected readonly Func<TIn, TOut> _converter;
-
-        protected DynamicValue<TIn> _value;
-        protected event Action<TOut> _callback;
-
-        public DynamicValue(Func<TIn, TOut> converter, DynamicValue<TIn> value)
-        {
-            _converter = converter;
-            _value = value;
-        }
-
-        public TOut Value
-        {
-            get => GetValue();
-        }
-
-        public void AddListener(Action<TOut> callback)
-        {
-            if (_callback == null)
-            {
-                _value.AddListener(OnValueChanged);
-            }
-
-            _callback += callback;
-        }
-
-        public void RemoveListener(Action<TOut> callback)
-        {
-            _callback -= callback;
-
-            if (_callback == null)
-            {
-                _value.RemoveListener(OnValueChanged);
-            }
-        }
-
-        public void RemoveAllListeners()
-        {
-            _callback = null;
-            _value.RemoveListener(OnValueChanged);
-        }
-
-        public virtual TOut GetValue()
-        {
-            return _converter(_value);
-        }
-
-        protected void Invoke(TOut value)
-        {
-            _callback?.Invoke(value);
-        }
-
-        protected void Invoke(TIn value)
-        {
-            Invoke(_converter(value));
-        }
-
-        public void Invoke()
-        {
-            Invoke(_value);
-        }
-
-        protected void OnValueChanged(TIn value)
-        {
-            Invoke(value);
-        }
-
-        public static implicit operator TOut(DynamicValue<TIn, TOut> value)
-        {
-            return value.Value;
-        }
-
-        private bool Equals(DynamicValue<TIn, TOut> other)
-        {
-            return (
-                other != null &&
-                (ReferenceEquals(this, other) || Equals(this.Value, other.Value))
-            );
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as DynamicValue<TIn, TOut>);
         }
 
         public override int GetHashCode()
